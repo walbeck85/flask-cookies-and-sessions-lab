6b05e666ecdf17f250472c5f0f9c4c0d5306c020
Laura Berge <lauraeberge@gmail.com>
2025-05-06 09:05:07 -0500

    convert to marshmallow from serializermixin

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article, User, ArticleSchema, UserSchema
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -22,13 +22,12 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
     pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
     pass
+
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from marshmallow import Schema, fields
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,7 +8,7 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Article(db.Model, SerializerMixin):
+class Article(db.Model):
     __tablename__ = 'articles'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -20,17 +20,35 @@ class Article(db.Model, SerializerMixin):
     date = db.Column(db.DateTime, server_default=db.func.now())
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='articles')
 
     def __repr__(self):
         return f'Article {self.id} by {self.author}'
 
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    articles = db.relationship('Article', backref='user')
+    articles = db.relationship('Article', back_populates='user')
 
     def __repr__(self):
         return f'User {self.name}, ID {self.id}'
+
+class ArticleSchema(Schema):
+    id = fields.Int()
+    author = fields.String()
+    title = fields.String()
+    content = fields.String()
+    preview = fields.String()
+    minutes_to_read = fields.Int()
+    date = fields.DateTime()
+
+    user = fields.Nested(lambda: UserSchema(exclude=("articles",)))
+
+class UserSchema(Schema):
+    id = fields.Int()
+    name = fields.String()
+
+    articles = fields.Nested(ArticleSchema(exclude=("user",)))
